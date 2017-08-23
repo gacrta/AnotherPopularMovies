af127a07dddeddca832bd15b981236978bf500c6
Gabriel Almeida/LGESP RGabrielc.almeidaD MC SW(gabrielc.almeida@lge.com) <gabrielc.almeida@lge.com>
2017-08-23 10:13:24 -0300

    [APM] Initial commit
    [desc] Started with blank activity

---
+++ README.md
@@ -0,0 +1,4 @@
+# Another Popular Movies
+
+This is a learning Application for Udacity's Nanodegree.
+It will fetches data from MoviesDB API and show to users. They can click a card and receive more info about a specific title.
---
+++ app/src/main/java/com/learning/gacrta/anotherpopularmovies/MoviesCatalog.java
@@ -0,0 +1,13 @@
+package com.learning.gacrta.anotherpopularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesCatalog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_catalog);
+    }
+}
